2abbc0944066bafe9065dcfa59cce98be0a4d430
benjyiw <benwesemann10@gmail.com>
2018-04-07 12:11:13 -0600

    Initial commit

---
+++ src/simon/Score.java
@@ -0,0 +1,27 @@
+package simon;
+
+public class Score {
+	public boolean IfHighScore(int newScore) {
+		//TODO
+		return false;
+	}
+
+	public void AddHighScore(int newScore, String initials) {
+		//TODO
+	}
+
+	public int GetHighScore() {
+		//TODO
+		return 0;
+	}
+
+	public int[] GetTopFiveScores() {
+		//TODO
+		return null;
+	}
+
+	public void SaveScores() {
+		//TODO
+	}
+
+}
---
+++ src/simon/Sequence.java
@@ -0,0 +1,24 @@
+package simon;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sequence {
+	private List<Integer> generatedSequence = new ArrayList<>();
+	private List<Integer> userGuessSequence = new ArrayList<>();
+	private int score = 0;
+
+	public int[] GetSequence() {
+		//TODO
+		return null;
+	}
+
+	public boolean UserInput(int input) {
+		//TODO
+		return false;
+	}
+	public int GetCurrentScore() {
+		return score;
+	}
+	
+}
