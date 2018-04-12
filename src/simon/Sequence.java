3af9ea02e1286c99ea07525e78c665378e63aa05
PlayerOneTrent <PlayerOneTrent@192.168.146.201>
2018-04-12 17:47:28 -0600

    Beta version of a sequence. Changed a few things, let's see if it works.

--- src/simon/Sequence.java
+++ src/simon/Sequence.java
@@ -2,22 +2,37 @@ package simon;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Sequence {
-	private List<Integer> generatedSequence = new ArrayList<>();
-	private List<Integer> userGuessSequence = new ArrayList<>();
-	private int score = 0;
+	public static List<Character> generatedSequence = new ArrayList<>();
+	public static List<Character> userGuessSequence = new ArrayList<>();
+	public int score = 0;
+	public static char[] rgby = {'Y', 'B', 'R', 'G'};
+	public static Random rand = new Random();
 
-	public int[] GetSequence() {
-		//TODO
-		return null;
+	public static List<Character> getSequence() {
+		generatedSequence.clear();
+		for(int i = 0; i < 4; i++) {
+			generatedSequence.add(rgby[rand.nextInt(3)]);
+		}
+		return generatedSequence;
 	}
 
-	public boolean UserInput(int input) {
-		//TODO
+	public boolean userInput(char input) {
+		userGuessSequence.add(input);
+		if(generatedSequence.get(userGuessSequence.size()-1) == userGuessSequence.get(userGuessSequence.size()-1)) {
+			return true;
+		} else {
 		return false;
+		}
 	}
-	public int GetCurrentScore() {
+	
+	public static void addSequence() {
+		generatedSequence.add(rgby[rand.nextInt(3)]);
+	}
+	
+	public int getCurrentScore() {
 		return score;
 	}
 	
