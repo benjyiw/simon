632dbdc623bbd8e17633b3c900d35680378bf460
benjyiw <benwesemann10@gmail.com>
2018-04-17 18:40:06 -0600

    adding jordans initial work

--- src/simon/Simon.java
+++ src/simon/Simon.java
@@ -6,8 +6,18 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLayeredPane;
+import javax.swing.JButton;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.border.BevelBorder;
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import java.awt.FlowLayout;
 
-public class Simon extends JFrame {
+public class SimonApp extends JFrame {
 
 	private JPanel contentPane;
 
@@ -18,7 +28,7 @@ public class Simon extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Simon frame = new Simon();
+					SimonApp frame = new SimonApp();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -30,13 +40,77 @@ public class Simon extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public Simon() {
+	public SimonApp() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 953, 732);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenuItem HiScoreBtn = new JMenuItem("Check High Scores");
+		HiScoreBtn.setHorizontalTextPosition(SwingConstants.CENTER);
+		HiScoreBtn.setBackground(new Color(124, 252, 0));
+		HiScoreBtn.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, new Color(192, 192, 192), new Color(64, 64, 64)));
+		HiScoreBtn.setHorizontalAlignment(SwingConstants.CENTER);
+		HiScoreBtn.setFont(new Font("Segoe UI", Font.BOLD, 15));
+		menuBar.add(HiScoreBtn);
+		
+		JMenuItem PlayBtn = new JMenuItem("Start New Game");
+		PlayBtn.setHorizontalTextPosition(SwingConstants.CENTER);
+		PlayBtn.setBackground(new Color(255, 69, 0));
+		PlayBtn.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, Color.LIGHT_GRAY, Color.DARK_GRAY));
+		PlayBtn.setHorizontalAlignment(SwingConstants.CENTER);
+		PlayBtn.setFont(new Font("Segoe UI", Font.BOLD, 15));
+		menuBar.add(PlayBtn);
+		
+		JMenuItem QuitBtn = new JMenuItem("Quit");
+		QuitBtn.setHorizontalTextPosition(SwingConstants.CENTER);
+		QuitBtn.setBackground(new Color(255, 255, 0));
+		QuitBtn.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, Color.LIGHT_GRAY, Color.BLACK));
+		QuitBtn.setHorizontalAlignment(SwingConstants.CENTER);
+		QuitBtn.setFont(new Font("Segoe UI", Font.BOLD, 15));
+		menuBar.add(QuitBtn);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JLayeredPane gamePlane = new JLayeredPane();
+		contentPane.add(gamePlane, BorderLayout.CENTER);
+		
+		JButton BG = new JButton("");
+		BG.setBackground(Color.WHITE);
+		BG.setBounds(0, 0, 911, 582);
+		gamePlane.add(BG);
+		
+		JButton SimonGame = new JButton("");
+		gamePlane.setLayer(SimonGame, 1);
+		SimonGame.setIcon(new ImageIcon(SimonApp.class.getResource("/simonGame/Simon Game.png")));
+		SimonGame.setBounds(294, 114, 308, 326);
+		gamePlane.add(SimonGame);
+		
+		JButton GreenLight = new JButton("");
+		GreenLight.setBackground(new Color(173, 255, 47));
+		gamePlane.setLayer(GreenLight, 2);
+		GreenLight.setBounds(308, 128, 110, 112);
+		gamePlane.add(GreenLight);
+		
+		JButton RedLight = new JButton("");
+		RedLight.setBackground(new Color(255, 0, 0));
+		gamePlane.setLayer(RedLight, 2);
+		RedLight.setBounds(477, 128, 110, 112);
+		gamePlane.add(RedLight);
+		
+		JButton BlueLight = new JButton("");
+		BlueLight.setBackground(new Color(0, 191, 255));
+		gamePlane.setLayer(BlueLight, 2);
+		BlueLight.setBounds(311, 315, 110, 112);
+		gamePlane.add(BlueLight);
+		
+		JButton YellowLight = new JButton("");
+		YellowLight.setBackground(Color.YELLOW);
+		gamePlane.setLayer(YellowLight, 2);
+		YellowLight.setBounds(477, 315, 110, 112);
+		gamePlane.add(YellowLight);
 	}
-
 }
